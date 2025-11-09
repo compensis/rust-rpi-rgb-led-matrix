9df6d9c5b75c3e63207586b704abfb32011e34dd
Philipp Compensis <github@0xg.de>
2025-11-09 19:24:54 +0100

    Add text example

---
+++ rpi-led-matrix/examples/text.rs
@@ -0,0 +1,33 @@
+/// Example showing some basic usage of the C++ library.
+use clap::{crate_version, App};
+use rpi_led_matrix::{args, LedFont, LedColor, LedMatrix};
+
+const DELAY: std::time::Duration = std::time::Duration::from_secs(5);
+
+fn main() {
+    let app = args::add_matrix_args(
+        App::new("C++ Library Example")
+            .about("shows basic usage of matrix arguments")
+            .version(crate_version!())
+    );
+    let matches = app.get_matches();
+    let (options, runtime_options) = args::matrix_options_from_args(&matches);
+
+    let matrix = LedMatrix::new(Some(options), Some(runtime_options)).unwrap();
+    let mut canvas = matrix.offscreen_canvas();
+    let color = LedColor {
+        red: 255,
+        green: 255,
+        blue: 255,
+    };
+
+    let font_file = std::path::Path::new("./rpi-led-matrix-sys/cpp-library/fonts/5x8.bdf");
+    let font = LedFont::new(font_file).unwrap();
+    let baseline = font.height().unwrap();
+    
+    canvas.clear();
+    canvas.draw_text(&font, "Halle World", 0, baseline, &color, 0, false);
+    let _ = matrix.swap(canvas);
+
+    std::thread::sleep(DELAY);
+}
