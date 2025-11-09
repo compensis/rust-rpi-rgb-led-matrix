6a8691111f8e95dec76d9d8350c5acea3fa44128
Philipp Compensis <github@0xg.de>
2025-11-09 22:20:30 +0100

    Add text wrapping example

---
+++ rpi-led-matrix/examples/textwrap.rs
@@ -0,0 +1,34 @@
+/// Example showing some basic usage of the C++ library.
+use clap::{crate_version, App};
+use rpi_led_matrix::{args, LedFont, LedColor, LedMatrix};
+
+const DELAY: std::time::Duration = std::time::Duration::from_secs(5);
+
+fn main() {
+    let app = args::add_matrix_args(
+        App::new("C++ Library Example")
+            .about("shows basic usage of matrix arguments")
+            .version(crate_version!())
+    );
+    let matches = app.get_matches();
+    let (options, runtime_options) = args::matrix_options_from_args(&matches);
+
+    let matrix = LedMatrix::new(Some(options), Some(runtime_options)).unwrap();
+    let mut canvas = matrix.offscreen_canvas();
+    let (width, _height) = canvas.canvas_size();
+    let color = LedColor {
+        red: 255,
+        green: 255,
+        blue: 255,
+    };
+
+    let font_file = std::path::Path::new("./rpi-led-matrix-sys/cpp-library/fonts/Grand9K-Pixel.bdf");
+    let font = LedFont::new(font_file).unwrap();
+    let text = "To be, or not to be: that is the question";
+    
+    canvas.clear();
+    canvas.draw_text_wrapped(&font, text, 0, 0, width, &color, 0, 0);
+    let _ = matrix.swap(canvas);
+
+    std::thread::sleep(DELAY);
+}
